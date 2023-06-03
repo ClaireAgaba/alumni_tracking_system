8efb68a43db892ab22e88f4f72fb3527882109b6
Agaba Claire Linda <agabaclaire70@gmail.com>
2023-06-03 21:25:34 +0300

    changes

--- alumni/urls.py
+++ alumni/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.loginPage, name='login'),
+    path('doLogin/', views.doLogin, name="doLogin"),
+    path('get_user_details/', views.get_user_details, name="get_user_details"),
+    path('logout_user/', views.logout_user, name="logout_user"),
 ]
