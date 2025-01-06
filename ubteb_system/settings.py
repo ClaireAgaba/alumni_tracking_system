63d4e0d2963810755c26ae50737531114dc52e34
ClaireAgaba <agabaclaire70@gmail.com>
2025-01-06 21:55:17 +0300

    fix: Update database configuration and Dockerfile for proper deployment

--- ubteb_system/settings.py
+++ ubteb_system/settings.py
@@ -93,10 +93,11 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
